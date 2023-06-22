b3fecbf8a713dc2af7413bfecbad759a7081c779
unknown <david@southfloridaathleticclub.com>
2023-06-22 18:55:46 -0400

    smart recruiters

--- pkg/handler.py
+++ pkg/handler.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 
 from pkg.sites.workdayjobs import click_add_fields, click_hidden_button, click_save_and_continue, enter_login, get_correct_year, handle_inputs, perform_action
+from pkg.utils import handle_calendar_select, handle_smart_autocomplete_fields
 
 class Handler:
     def __init__(self, bot):
@@ -18,12 +19,8 @@ class Handler:
         if "smartrecruiters" in job['apply']:
             self.upload_smartrecruiters_resume(driver=self.driver)
         try:
-            if "greenhouse" in job['apply']:
-                self.greenhouse(driver=self.driver, data=self.data, values=self.values)
-            elif "smartrecruiters" in job['apply']:
-                self.smartrecruiters(driver=self.driver, data=self.data, values=self.values)
-            elif "lever" in job['apply']:
-                self.lever(driver=self.driver, data=self.data, values=self.values)
+            if "smartrecruiters" in job['apply']:
+                self.handle_smartrecruiters()
             elif "underdog.io" in job['apply']:
                 self.underdog(driver=self.driver, data=self.data, values=self.values)
             else:
@@ -113,3 +110,106 @@ class Handler:
 
         # Save & Continue
         click_save_and_continue(self.bot.driver)
+    
+    def handle_smartrecruiters(self):
+        sleep(1)
+
+        try:
+            # Delete Resume Fields
+            field_options = self.bot.driver.find_elements(By.XPATH, '//button[@aria-label="See options"]')
+            for option in field_options:
+                option.click()
+
+                # Click 'Delete Position in Dropdown & Wait for Dialogue Box to Open'
+                delete_position = self.bot.driver.find_element(By.XPATH, '//button[@data-test="entry-delete"]')
+                delete_position.click()
+                sleep(2)
+
+                yes_button = self.bot.driver.find_element(By.XPATH, '//mat-dialog-container/oc-yes-no/div/div/button[2]')
+                yes_button.click()
+
+            sections = self.bot.driver.find_elements(By.CLASS_NAME, 'form-section')
+
+            for section in sections:
+                section_header = section.find_element(By.TAG_NAME, 'h3').get_attribute('innerText')
+                
+                if "Experience" in section_header:
+                    button = section.find_element(By.TAG_NAME, 'button')
+                    button.click()
+
+                    form_fields = section.find_elements(By.CLASS_NAME, 'form-control')
+
+                    for field in form_fields:
+                        label = field.find_element(By.TAG_NAME, 'label').get_attribute('innerText')
+
+                    if "Title" in label:
+                        input_element = field.find_element(By.TAG_NAME, 'input')
+                        handle_smart_autocomplete_fields(input_element, self.bot.data['title'])
+                    if "Company" in label:
+                        input_element = field.find_element(By.TAG_NAME, 'input')
+                        handle_smart_autocomplete_fields(input_element, self.bot.data['currentCompany'])
+                    if "Office location" in label:
+                        input_element = field.find_element(By.CLASS_NAME, 'sr-location-autocomplete')
+                        handle_smart_autocomplete_fields(input_element, self.bot.data['companyLocation'])
+                    if "Description" in label:
+                        input_element = field.find_element(By.TAG_NAME, 'textarea')
+                        handle_smart_autocomplete_fields(input_element, self.bot.data['jobDescription'])
+                    if "From" in label:
+                        work_here = field.find_element(By.XPATH, '//*[@data-test="experience-current"]')
+                        work_here.click()
+                        
+                        calendar_button = field.find_element(By.XPATH, '//button[@aria-label="Open calendar"]')
+                        calendar_button.click()
+
+                        # Select Year
+                        handle_calendar_select(self.bot.driver, self.bot.data['jobStartYear'])
+                        # Select Month
+                        handle_calendar_select(self.bot.driver, self.bot.data['jobStartMonth'])
+
+                        # Save
+                        save_button = field.find_element(By.XPATH, '//button[@data-test="experience-save"]')
+                        save_button.click()
+
+                if "Education" in section_header:
+                    button = section.find_element(By.TAG_NAME, 'button')
+                    button.click()
+
+                    form_fields = section.find_elements(By.CLASS_NAME, 'form-control')
+
+                    for field in form_fields:
+                        label = field.find_element(By.TAG_NAME, 'label').get_attribute('innerText')
+
+                        if "Institution" in label:
+                            input_element = field.find_element(By.TAG_NAME, 'input')
+                            handle_smart_autocomplete_fields(input_element, self.bot.data['school'])
+                        if "Major" in label:
+                            input_element = field.find_element(By.TAG_NAME, 'input')
+                            input_element.send_keys(self.bot.data['discipline'])
+                        if "Degree" in label:
+                            input_element = field.find_element(By.TAG_NAME, 'input')
+                            input_element.send_keys(self.bot.data['degree'])
+                        if "School location" in label:
+                            input_element = field.find_element(By.CLASS_NAME, 'sr-location-autocomplete')
+                            handle_smart_autocomplete_fields(input_element, self.bot.data['schoolLocation'])
+                        if "Description" in label:
+                            input_element = field.find_element(By.TAG_NAME, 'textarea')
+                            handle_smart_autocomplete_fields(input_element, self.bot.data['degreeDescription'])
+                        if "From" in label:
+                            work_here = field.find_element(By.XPATH, '//*[@data-test="education-current"]')
+                            work_here.click()
+                            
+                            calendar_button = field.find_element(By.XPATH, '//button[@aria-label="Open calendar"]')
+                            calendar_button.click()
+
+                            # Select Year
+                            handle_calendar_select(self.bot.driver, self.bot.data['universityStartDate'])
+                            
+                            # Select Month
+                            handle_calendar_select(self.bot.driver, self.bot.data['universityEndDate'])
+
+                            # Save
+                            save_button = field.find_element(By.XPATH, '//button[@data-test="education-save"]')
+                            save_button.click()
+        except BaseException as err:
+            print(err)
+            pass
