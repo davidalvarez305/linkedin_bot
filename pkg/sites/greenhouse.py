0199492c3e878486eedb9ef707c47f5c3bb0e59e
davidalvarez305 <david@southfloridaathleticclub.com>
2023-07-04 04:10:22 -0400

    greenhouse

--- pkg/sites/greenhouse.py
+++ pkg/sites/greenhouse.py
@@ -1,14 +1,14 @@
 from time import sleep
-from ..utils import field_match, find_fields_by_label, handle_input_field, handle_select_child_options
+from ..utils import field_match, handle_input_field, handle_select_child_options
 from selenium.webdriver.common.by import By
 
-def handle_greenhouse_autocomplete(driver, data, field_name):
+def handle_input(input, user_data, driver):
+    input.send_keys(user_data)
+    sleep(1.5)
+    drop_element = driver.find_element(By.ID, "selectedOption")
+    drop_element.click()
 
-    def handle_input(input, user_data):
-        input.send_keys(user_data)
-        sleep(1.5)
-        drop_element = driver.find_element(By.ID, "selectedOption")
-        drop_element.click()
+def handle_greenhouse_autocomplete(driver, data, field_name):
 
     autocomplete_fields = driver.find_elements(By.XPATH, '//div[@class="select2-search"]')
 
@@ -16,22 +16,22 @@ def handle_greenhouse_autocomplete(driver, data, field_name):
         try:
             # Handle Autocomplete Fields
             if "School" in field_name:
-                handle_input(field.find_element(By.TAG_NAME, "input"), data['user']['school'])
+                handle_input(field.find_element(By.TAG_NAME, "input"), data['school'], driver)
             if "Are you 18" in field_name:
-                handle_input(field.find_element(By.TAG_NAME, "input"), "Yes")
+                handle_input(field.find_element(By.TAG_NAME, "input"), "Yes", driver)
             if "authorized" in field_name:
-                handle_input(field.find_element(By.TAG_NAME, "input"), data['user']['workAuthorization'])
+                handle_input(field.find_element(By.TAG_NAME, "input"), data['workAuthorization'], driver)
             if "Degree" in field_name or "degree" in field_name:
-                handle_input(field.find_element(By.TAG_NAME, "input"), "Associate")
+                handle_input(field.find_element(By.TAG_NAME, "input"), "Associate", driver)
             if "Discipline" in field_name:
-                handle_input(field.find_element(By.TAG_NAME, "input"), "Business")
+                handle_input(field.find_element(By.TAG_NAME, "input"), "Business", driver)
         except BaseException:
             continue
 
-def handle_hidden_field(field_name, element, driver, data, values):
+def handle_hidden_field(field_name, element, driver, data, questions):
     select_fields = element.find_elements(By.TAG_NAME, 'select')
 
-    for value in values:
+    for question in questions:
         if "Were you referred by" in field_name:
             handle_select_child_options(element, "No")
         if "resume" in field_name.lower():
@@ -39,11 +39,11 @@ def handle_hidden_field(field_name, element, driver, data, values):
         if "require" in field_name and "immigration" in field_name:
             btns = driver.find_elements(By.CLASS_NAME, "application-answer-alternative")
             for btn in btns:
-                if field_match(btn.get_attribute('textContent'), data=data['user']['immigrationSponsorship']):
+                if field_match(btn.get_attribute('textContent'), data=data['immigrationSponsorship']):
                     btn.click()
-        if any(substr in field_name.lower() for substr in value['question']):
+        if any(substr in field_name.lower() for substr in question['question']):
             if len(select_fields) > 0:
-                handle_select_child_options(element, data['user'][f"{value['data']}"])
+                handle_select_child_options(element, data[f"{question['data']}"])
             else:
                 x_path = './label/input[@type="text"]'
-                handle_input_field(element, data['user'][f"{value['data']}"], x_path)
+                handle_input_field(element, data[f"{question['data']}"], x_path)
