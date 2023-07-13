6e7e70b50c62890334bf6285c09921edcb67decc
davidalvarez305 <david@southfloridaathleticclub.com>
2023-07-12 23:45:47 -0400

    fixing handling of experience

--- pkg/handler.py
+++ pkg/handler.py
@@ -7,6 +7,7 @@ from pkg.sites.bamboo import handle_select_div
 from pkg.sites.greenhouse import handle_greenhouse_autocomplete, handle_hidden_field
 from pkg.sites.lever import find_lever_elements, handle_lever_fields
 from pkg.sites.underdog import auto_complete
+from pathlib import Path
 
 from pkg.sites.workdayjobs import click_add_fields, click_hidden_button, click_save_and_continue, enter_login, get_correct_year, handle_inputs, perform_action
 from pkg.utils import click_preapplication_button, find_fields_by_label, handle_calendar_select, handle_pre_application_button, handle_smart_autocomplete_fields, handle_textarea
@@ -33,7 +34,7 @@ class Handler:
                     return
                 elif "smartrecruiters" in job.get('apply'):
                     resume_upload = self.bot.driver.find_element(By.XPATH, '//input[@class="file-upload-input"]')
-                    resume_upload.send_keys(os.path.join(os.path.dirname(os.path.abspath('./linkedin_bot')), self.bot.data['resume']))
+                    resume_upload.send_keys(os.path.join(Path(__file__).parent.parent, self.bot.data['resume']))
                     sleep(5)
                     self.handle_smartrecruiters()
                     return
@@ -153,86 +154,98 @@ class Handler:
 
             sections = self.bot.driver.find_elements(By.CLASS_NAME, 'form-section')
 
+            if len(sections) == 0:
+                raise Exception('No sections found.')
+
             for section in sections:
-                section_header = section.find_element(By.TAG_NAME, 'h3').get_attribute('innerText')
-                
-                if "Experience" in section_header:
-                    button = section.find_element(By.TAG_NAME, 'button')
-                    button.click()
+                try:
+                    section_header = section.find_element(By.TAG_NAME, 'h2').get_attribute('innerText')
+                    print('Handling section: ', section_header)
+                    
+                    if "Experience" in section_header:
+                        button = section.find_element(By.TAG_NAME, 'button')
+                        button.click()
 
-                    form_fields = section.find_elements(By.CLASS_NAME, 'form-control')
+                        form_fields = section.find_elements(By.CLASS_NAME, 'form-control')
 
-                    for field in form_fields:
-                        label = field.find_element(By.TAG_NAME, 'label').get_attribute('innerText')
+                        for field in form_fields:
+                            try:
+                                label = field.find_element(By.TAG_NAME, 'label').get_attribute('innerText')
 
-                    if "Title" in label:
-                        input_element = field.find_element(By.TAG_NAME, 'input')
-                        handle_smart_autocomplete_fields(input_element, self.bot.data['title'])
-                    if "Company" in label:
-                        input_element = field.find_element(By.TAG_NAME, 'input')
-                        handle_smart_autocomplete_fields(input_element, self.bot.data['currentCompany'])
-                    if "Office location" in label:
-                        input_element = field.find_element(By.CLASS_NAME, 'sr-location-autocomplete')
-                        handle_smart_autocomplete_fields(input_element, self.bot.data['companyLocation'])
-                    if "Description" in label:
-                        input_element = field.find_element(By.TAG_NAME, 'textarea')
-                        handle_smart_autocomplete_fields(input_element, self.bot.data['jobDescription'])
-                    if "From" in label:
-                        work_here = field.find_element(By.XPATH, '//*[@data-test="experience-current"]')
-                        work_here.click()
-                        
-                        calendar_button = field.find_element(By.XPATH, '//button[@aria-label="Open calendar"]')
-                        calendar_button.click()
+                                if "Title" in label:
+                                    input_element = field.find_element(By.TAG_NAME, 'input')
+                                    handle_smart_autocomplete_fields(input_element, self.bot.data['title'])
+                                if "Company" in label:
+                                    input_element = field.find_element(By.TAG_NAME, 'input')
+                                    handle_smart_autocomplete_fields(input_element, self.bot.data['currentCompany'])
+                                if "Office location" in label:
+                                    input_element = field.find_element(By.CLASS_NAME, 'sr-location-autocomplete')
+                                    handle_smart_autocomplete_fields(input_element, self.bot.data['companyLocation'])
+                                if "Description" in label:
+                                    input_element = field.find_element(By.TAG_NAME, 'textarea')
+                                    handle_smart_autocomplete_fields(input_element, self.bot.data['jobDescription'])
+                                if "From" in label:
+                                    work_here = field.find_element(By.XPATH, '//*[@data-test="experience-current"]')
+                                    work_here.click()
+                                
+                                    calendar_button = field.find_element(By.XPATH, '//button[@aria-label="Open calendar"]')
+                                    calendar_button.click()
 
-                        # Select Year
-                        handle_calendar_select(self.bot.driver, self.bot.data['jobStartYear'])
-                        # Select Month
-                        handle_calendar_select(self.bot.driver, self.bot.data['jobStartMonth'])
+                                    # Select Year
+                                    handle_calendar_select(self.bot.driver, self.bot.data['jobStartYear'])
+                                    # Select Month
+                                    handle_calendar_select(self.bot.driver, self.bot.data['jobStartMonth'])
 
-                        # Save
-                        save_button = field.find_element(By.XPATH, '//button[@data-test="experience-save"]')
-                        save_button.click()
+                                    # Save
+                                    save_button = field.find_element(By.XPATH, '//button[@data-test="experience-save"]')
+                                    save_button.click()
+                            except BaseException as err:
+                                print(err)
+                                continue
 
-                if "Education" in section_header:
-                    button = section.find_element(By.TAG_NAME, 'button')
-                    button.click()
+                    if "Education" in section_header:
+                        button = section.find_element(By.TAG_NAME, 'button')
+                        button.click()
 
-                    form_fields = section.find_elements(By.CLASS_NAME, 'form-control')
+                        form_fields = section.find_elements(By.CLASS_NAME, 'form-control')
 
-                    for field in form_fields:
-                        label = field.find_element(By.TAG_NAME, 'label').get_attribute('innerText')
+                        for field in form_fields:
+                            label = field.find_element(By.TAG_NAME, 'label').get_attribute('innerText')
 
-                        if "Institution" in label:
-                            input_element = field.find_element(By.TAG_NAME, 'input')
-                            handle_smart_autocomplete_fields(input_element, self.bot.data['school'])
-                        if "Major" in label:
-                            input_element = field.find_element(By.TAG_NAME, 'input')
-                            input_element.send_keys(self.bot.data['discipline'])
-                        if "Degree" in label:
-                            input_element = field.find_element(By.TAG_NAME, 'input')
-                            input_element.send_keys(self.bot.data['degree'])
-                        if "School location" in label:
-                            input_element = field.find_element(By.CLASS_NAME, 'sr-location-autocomplete')
-                            handle_smart_autocomplete_fields(input_element, self.bot.data['schoolLocation'])
-                        if "Description" in label:
-                            input_element = field.find_element(By.TAG_NAME, 'textarea')
-                            handle_smart_autocomplete_fields(input_element, self.bot.data['degreeDescription'])
-                        if "From" in label:
-                            work_here = field.find_element(By.XPATH, '//*[@data-test="education-current"]')
-                            work_here.click()
-                            
-                            calendar_button = field.find_element(By.XPATH, '//button[@aria-label="Open calendar"]')
-                            calendar_button.click()
+                            if "Institution" in label:
+                                input_element = field.find_element(By.TAG_NAME, 'input')
+                                handle_smart_autocomplete_fields(input_element, self.bot.data['school'])
+                            if "Major" in label:
+                                input_element = field.find_element(By.TAG_NAME, 'input')
+                                input_element.send_keys(self.bot.data['discipline'])
+                            if "Degree" in label:
+                                input_element = field.find_element(By.TAG_NAME, 'input')
+                                input_element.send_keys(self.bot.data['degree'])
+                            if "School location" in label:
+                                input_element = field.find_element(By.CLASS_NAME, 'sr-location-autocomplete')
+                                handle_smart_autocomplete_fields(input_element, self.bot.data['schoolLocation'])
+                            if "Description" in label:
+                                input_element = field.find_element(By.TAG_NAME, 'textarea')
+                                handle_smart_autocomplete_fields(input_element, self.bot.data['degreeDescription'])
+                            if "From" in label:
+                                work_here = field.find_element(By.XPATH, '//*[@data-test="education-current"]')
+                                work_here.click()
+                                
+                                calendar_button = field.find_element(By.XPATH, '//button[@aria-label="Open calendar"]')
+                                calendar_button.click()
 
-                            # Select Year
-                            handle_calendar_select(self.bot.driver, self.bot.data['universityStartDate'])
-                            
-                            # Select Month
-                            handle_calendar_select(self.bot.driver, self.bot.data['universityEndDate'])
+                                # Select Year
+                                handle_calendar_select(self.bot.driver, self.bot.data['universityStartDate'])
+                                
+                                # Select Month
+                                handle_calendar_select(self.bot.driver, self.bot.data['universityEndDate'])
 
-                            # Save
-                            save_button = field.find_element(By.XPATH, '//button[@data-test="education-save"]')
-                            save_button.click()
+                                # Save
+                                save_button = field.find_element(By.XPATH, '//button[@data-test="education-save"]')
+                                save_button.click()
+                except BaseException as err:
+                    print(f'Error: {err}')
+                    continue
         except BaseException as err:
             raise Exception(f'Failed to handle SmartRecruiters: {err}')
     
